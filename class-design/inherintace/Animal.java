89aabf555e4bc20e33c5780fdf7639ebd8668a95
Brayan Espinoza <bjespinozah@gmail.com>
2017-10-16 02:54:55 -0500

    Adding folder inherentace

---
+++ class-design/inherintace/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+  private int age;
+  private String name;
+
+  public Animal(int age) {
+    super();
+    this.age = age;
+  }
+  public Animal(int age) {
+    super();
+    this.age = age;
+    this.name = null;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public int getAge() {
+    return age;
+  }
+}
---
+++ class-design/inherintace/Zebra.java
@@ -0,0 +1,9 @@
+public class Zebra {
+  public Zebra(int age) {
+    super(age);
+  }
+
+  public Zebra() {
+    this(4);
+  }
+}
